a83ab0f0da8f39f0f7a233d6c9528ff0ce94edab
gvasques77-sys <gvasques77@gmail.com>
2026-07-19 00:24:04 -0400

    Add consent-aware Google Analytics

--- components/cookie-consent-config.ts
+++ components/cookie-consent-config.ts
@@ -1,4 +1,4 @@
-export const COOKIE_CONSENT_NAME = "cactus_cookie_consent_v1";
+export const COOKIE_CONSENT_NAME = "cactus_cookie_consent_v2";
 export const COOKIE_CONSENT_MAX_AGE = 60 * 60 * 24 * 180;
 export const COOKIE_CONSENT_EVENT = "cactus:cookie-consent";
 
